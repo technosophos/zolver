0b3aee6b844b299c7de591280b9680530d0c7929
Matt Butcher <technosophos@gmail.com>
2017-04-17 18:35:49 -0600

    Add acid.js

---
+++ acid.js
@@ -0,0 +1,21 @@
+// Acid CI/CD
+
+// We need to simulate a Go environment
+gopath = "/go";
+localPath = gopath + "/src/github.com/" + pushRecord.repository.name;
+
+// Define a single build step:
+j = new Job("test-zolver");
+j.image = "technosophos/acid-go:latest";
+j.env = {
+  "DEST_PATH": localPath,
+  "GOPATH": gopath
+};
+
+j.tasks = [
+  "go get github.com/Masterminds/glide"
+  "make bootstrap",
+  "make test"
+];
+
+j.run(pushRecord).waitUntilDone()
