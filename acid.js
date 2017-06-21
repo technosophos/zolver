62727511e4c87b2d8a5f0ea9d0288bb74ce7dc2d
Matt Butcher <matt.butcher@microsoft.com>
2017-06-20 21:56:05 -0600

    update acid.js

--- acid.js
+++ acid.js
@@ -1,22 +1,24 @@
 // Acid CI/CD
-console.log("===> Building " + pushRecord.repository.full_url + " " + pushRecord.head_commit.id);
+events.push = function(e) {
+  console.log("===> Building " + e.repo.cloneURL + " " + e.commit);
 
-// We need to simulate a Go environment
-gopath = "/go";
-localPath = gopath + "/src/github.com/" + pushRecord.repository.full_name;
+  // We need to simulate a Go environment
+  gopath = "/go";
+  localPath = gopath + "/src/github.com/" + e.repo.name;
 
-// Define a single build step:
-j = new Job("test-zolver");
-j.image = "technosophos/acid-go:latest";
-j.env = {
-  "DEST_PATH": localPath,
-  "GOPATH": gopath
-};
+  // Define a single build step:
+  j = new Job("test-zolver");
+  j.image = "technosophos/acid-go:latest";
+  j.env = {
+    "DEST_PATH": localPath,
+    "GOPATH": gopath
+  };
 
-j.tasks = [
-  "go get github.com/Masterminds/glide",
-  "make bootstrap",
-  "make test"
-];
+  j.tasks = [
+    "go get github.com/Masterminds/glide",
+    "make bootstrap",
+    "make test"
+  ];
 
-j.run(pushRecord)
+  j.run()
+}
